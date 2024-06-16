c673dae0836486695f7f4c7da792dfe0aa0a0df8
Zatt010 <afuheho1@gmail.com>
2024-06-16 19:16:40 -0400

    About and browser scenaros in 0gmo

--- features/0gmo.feature
+++ features/0gmo.feature
@@ -21,4 +21,27 @@ Feature: GMO Online Catalog
       | Hiking Boots            | $ 109.90 |
       | Back Country Shorts     | $ 24.95  |
 
+  Scenario: Viewing About the GMO Site
+    When I click on "About the GMO Site"
+    Then I should see the About the GMO Site page
+    And I should see the text "This is a sample online commerce application. It is not real."
+    And I should see the text "Green Mountain Outpost is a fictitious company."
+    And I should see the privacy and security warning
 
+
+  Scenario: Verifying Browser Test Page contents
+    When I press "Browser Test Page"
+    Then I should see "All Browsers Are Not Created Equal"
+    And I should see the following browser table contents:
+      | Internet Explorer 3.0        |
+      | Netscape Navigator 3.0       |
+    And I should see the following elements:
+      | Blinking Text               |
+      | dot bullets                 |
+      | circle bullets              |
+      | square bullets              |
+    And I should see the following elements in the side table:
+      | Colored Horizontal Rules    |
+      | Definition Text Style       |
+      | Trademarks are important    |
+      | Table Properties            |
--- features/step_definitions/0gmo_steps.rb
+++ features/step_definitions/0gmo_steps.rb
@@ -1,10 +1,15 @@
-
 Then(/^I should see the GMO Online Catalog page$/) do
   expect(page).to have_current_path('https://demo.borland.com/gmopost/online-catalog.htm')
 end
 
-Then(/^I should see "These are the items currently available through our online catalog"$/) do
+# Paso específico para verificar el mensaje "These are the items currently available through our online catalog"
+Then(/^I should see the catalog availability message$/) do
   expect(page).to have_selector('body > form > table > tbody > tr:nth-child(1) > td', text: 'These are the items currently available through our online catalog')
+end
+
+# Paso general para verificar cualquier texto en la página
+Then(/^I should see the text "(.*?)"$/) do |text|
+  expect(page).to have_content(text)
 end
 
 Then(/^I should see "([^"]*)" with price "\$ ([\d\.]+)"$/) do |item_name, price|
@@ -20,4 +25,49 @@ Then(/^I should see "([^"]*)" with price "\$ ([\d\.]+)"$/) do |item_name, price|
 
   price_selector = "body > form > table > tbody > tr:nth-child(2) > td > div > center > table > tbody > tr:nth-child(#{index + 2}) > td:nth-child(3)"
   expect(page).to have_selector(price_selector, text: "$ #{price}")
+end
+
+When(/^I click on "About the GMO Site"$/) do
+  find('body > form > div:nth-child(1) > center > table > tbody > tr > td:nth-child(2) > input[type=button]').click
+end
+
+Then(/^I should see the About the GMO Site page$/) do
+  expect(page).to have_content('This is a sample online commerce application. It is not real.')
+end
+
+# Paso para verificar cualquier texto en la página
+Then(/^I should see "([^"]+)"$/) do |text|
+  expect(page).to have_content(text)
+end
+
+Then(/^I should see the privacy and security warning$/) do
+  # Verifica la primera parte del texto
+  expect(page).to have_selector('font', text: 'For your privacy and security,')
+  # Verifica la segunda parte del texto
+  expect(page).to have_selector('font', text: 'DO NOT ENTER REAL BILLING OR SHIPPING INFORMATION')
+end
+When(/^I press "Browser Test Page"$/) do
+  find('input[name="bBrowserTest"]').click
+end
+
+Then(/^I should see the browser test page title$/) do
+  expect(page).to have_content('All Browsers Are Not Created Equal')
+end
+
+Then(/^I should see the following browser table contents:$/) do |table|
+  table.raw.flatten.each do |text|
+    expect(page).to have_selector('strong', text: text)
+  end
+end
+
+Then(/^I should see the following elements:$/) do |table|
+  table.raw.flatten.each do |text|
+    expect(page).to have_content(text)
+  end
+end
+
+Then(/^I should see the following elements in the side table:$/) do |table|
+  table.raw.flatten.each do |text|
+    expect(page).to have_content(text)
+  end
 end
